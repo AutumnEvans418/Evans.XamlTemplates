54f6df2a43a28ce3411c329096a07a7b662345d0
Chris Evans <chrisevans9629@gmail.com>
2020-01-10 16:36:34 -0600

    working on parser

---
+++ Evans.XamlTemplates/Evans.XamlTemplates/TamlParser.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Evans.XamlTemplates
+{
+    public enum Token
+    {
+        At,
+        Id,
+        ParenthesesOpen,
+        ParenthesesClose,
+        BracketOpen,
+        BracketClose,
+        Comma,
+        ForwardSlash,
+        Quote
+
+    }
+
+    //public class TamlParser
+    //{
+    //    public int Index { get; set; }
+    //    public IList<char> Characters { get; set; } = new List<char>();
+
+    //    public char? Peek(int offset = 0)
+    //    {
+    //        if (Characters.Count > Index + offset)
+    //            return Characters[Index + offset];
+    //        return null;
+    //    }
+
+    //    public void Move()
+    //    {
+    //        Index++;
+    //    }
+
+    //    public IEnumerable<Token> GetTokens(string code)
+    //    {
+    //        Characters = new List<char>(code);
+    //        Index = 0;
+    //        while (Peek() != null)
+    //        {
+                
+    //        }
+
+    //    }
+    //}
+}
