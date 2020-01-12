29be420739aed72afb819469db5eae25462a8d6c
Chris Evans <chrisevans9629@gmail.com>
2020-01-12 09:40:50 -0600

    updated msbuild

--- XamlTemplates.MSBuild/XamlTemplates.cs
+++ XamlTemplates.MSBuild/XamlTemplates.cs
@@ -7,35 +7,30 @@ using Microsoft.Build.Framework;
 
 namespace XamlTemplates.MSBuild
 {
-    public class MyExternalTask : Microsoft.Build.Utilities.Task
+    public class XamlTemplates : Microsoft.Build.Utilities.Task
     {
-        public string MyParameter { get; set; }
         public string Filter { get; set; } = "*.taml";
 
-        public string Namespace = 
+        public string Namespace { get; set; } =
             Assembly.GetEntryAssembly()?.GetName().Name ?? "Template";
         public override bool Execute()
         {
+            var templator = new Templator();
             var files = Directory.GetFiles(Directory.GetCurrentDirectory(), Filter);
-            var parser = new TamlParser();
-            var ast = new TamlAst();
-            var gen = new Generator();
-            gen.Namespace = Namespace;
+            
             foreach (var file in files)
             {
                 var content = File.ReadAllText(file);
-                var tokens = parser.GetTokens(content);
-                var t = ast.Evaluate(tokens);
 
-                var result = gen.Generate(t);
+                var result = templator.Generate(content, Namespace);
 
                 foreach (var generatedType in result)
                 {
                     var x = generatedType.Xaml;
                     var c = generatedType.CSharp;
 
-                    File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(),x.FileName), x.Content);
-                    File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(),c.FileName), c.Content);
+                    File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), x.FileName), x.Content);
+                    File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), c.FileName), c.Content);
                 }
             }
             return true;
