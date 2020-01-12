f1e08a235337ad60d9a3ca7107925d96c1ee01ec
Chris Evans <chrisevans9629@gmail.com>
2020-01-12 09:41:27 -0600

    added templator

---
+++ Compiler/Templator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Evans.XamlTemplates.Generator;
+
+namespace Evans.XamlTemplates
+{
+    public class Templator
+    {
+        TamlParser parser = new TamlParser();
+        TamlAst ast = new TamlAst();
+        Generator.Generator gen = new Generator.Generator();
+        public IEnumerable<GeneratedType> Generate(string code, string Namespace)
+        {
+            gen.Namespace = Namespace;
+            var tokens = parser.GetTokens(code);
+            var t = ast.Evaluate(tokens);
+            return gen.Generate(t);
+        }
+    }
+}
