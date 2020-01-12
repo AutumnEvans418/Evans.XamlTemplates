554c0da2bd4cfb2124cc06e2ceceeb3de5ecead9
Chris Evans <chrisevans9629@gmail.com>
2020-01-12 10:03:21 -0600

    xaml templates works

--- Compiler/Templator.cs
+++ Compiler/Templator.cs
@@ -1,19 +1,33 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Evans.XamlTemplates.Generator;
 
 namespace Evans.XamlTemplates
 {
+    public class Examples
+    {
+        public const string Basic = @"
+@LabelEntry(string Label,string Text)
+{
+	<StackLayout>
+		<Label Text=""@Label""/>
+		<Entry Text=""@Text""/>
+	</StackLayout>
+}";
+    }
+
+
     public class Templator
     {
         TamlParser parser = new TamlParser();
         TamlAst ast = new TamlAst();
         Generator.Generator gen = new Generator.Generator();
-        public IEnumerable<GeneratedType> Generate(string code, string Namespace)
+        public List<GeneratedType> Generate(string code, string Namespace)
         {
             gen.Namespace = Namespace;
             var tokens = parser.GetTokens(code);
             var t = ast.Evaluate(tokens);
-            return gen.Generate(t);
+            return gen.Generate(t).ToList();
         }
     }
 }
