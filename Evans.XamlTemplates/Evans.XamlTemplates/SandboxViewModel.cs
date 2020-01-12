ef33a1661737b31a4715fa37a256031c6c54e25d
Chris Evans <chrisevans9629@gmail.com>
2020-01-12 09:41:03 -0600

    adding sandbox

---
+++ Evans.XamlTemplates/Evans.XamlTemplates/SandboxViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Evans.XamlTemplates.Annotations;
+
+namespace Evans.XamlTemplates
+{
+
+
+    public class SandboxViewModel
+    {
+        
+    }
+}
