1e0c1eda259d1c9730fcf0967ae0755a01e8dc9f
Chris Evans <Cevans@peerlessproducts.com>
2020-01-13 07:44:34 -0600

    moved xamarin forms projects

--- packAndUpdate.ps1
+++ packAndUpdate.ps1
@@ -3,5 +3,5 @@ dotnet build .\XamlTemplates.MSBuild\XamlTemplates.MSBuild.csproj
 
 .\XamlTemplates.MSBuild\nuget.exe pack .\XamlTemplates.MSBuild\XamlTemplates.MSBuild.nuspec
 
-dotnet add .\Evans.XamlTemplates\Evans.XamlTemplates\Evans.XamlTemplates.csproj package XamlTemplates.MSBuild
+dotnet add .\Evans.XamlTemplates\Evans.XamlTemplates.csproj package XamlTemplates.MSBuild
 
