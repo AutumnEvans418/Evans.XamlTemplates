400ab3aebfe1cb47c8be5b4cbf8eb1ca38bef904
Chris Evans <chrisevans9629@gmail.com>
2020-01-14 19:33:57 -0600

    moved packages and update powershell script

--- packAndUpdate.ps1
+++ packAndUpdate.ps1
@@ -5,5 +5,7 @@ dotnet build .\XamlTemplates.MSBuild\XamlTemplates.MSBuild.csproj -c release
 
 .\XamlTemplates.MSBuild\nuget.exe pack .\XamlTemplates.MSBuild\XamlTemplates.MSBuild.nuspec
 
+Move-Item -Path ".\*.nupkg" -Destination ".\nugetResults" -Force -Verbose
+
 dotnet add .\Evans.XamlTemplates\Evans.XamlTemplates.csproj package XamlTemplates.MSBuild
 
