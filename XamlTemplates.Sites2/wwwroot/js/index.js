2fa5adefb5e46dd69667e46e4f17fca51d132416
Chris Evans <chrisevans9629@gmail.com>
2020-05-01 15:03:23 -0500

    moved javascript and got it working

---
+++ XamlTemplates.Sites2/wwwroot/js/index.js
@@ -0,0 +1,18 @@
+﻿hljs.initHighlightingOnLoad();
+
+window.replace = (id, content) => {
+    var result = document.querySelector('#' + id);
+    console.log("Found" + result);
+    if (result != null) {
+        result.textContent = content;
+    }
+    //result.content = content;
+};
+
+window.highlight = () => {
+    //hljs.initHighlightingOnLoad();
+    document.querySelectorAll('pre code').forEach((block) => {
+        hljs.highlightBlock(block);
+    });
+    console.log("highlighed!");
+};
